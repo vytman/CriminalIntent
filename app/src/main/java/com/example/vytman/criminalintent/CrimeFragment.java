3e50859a8de4547cc68da253b62da289223815e3
Vitor <vytman@gmail.com>
2018-07-23 21:33:35 +0100

    Created crime fragment

---
+++ app/src/main/java/com/example/vytman/criminalintent/CrimeFragment.java
@@ -0,0 +1,30 @@
+package com.example.vytman.criminalintent;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class CrimeFragment extends Fragment {
+
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
